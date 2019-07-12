8c42c25106ff58fd564a80b840de069a332c8c18
lwq1030 <787998189@qq.com>
2019-07-12 09:12:21 +0800

    alien now moves

--- alien.py
+++ alien.py
@@ -14,5 +14,17 @@ class Alien(Sprite):
         self.rect.y=self.rect.height
         self.x=float(self.rect.x)
 
+    def update(self):
+        #向右移动外星人
+        self.x+=(self.ai_settings.alien_speed_factor*self.ai_settings.fleet_direction)
+        self.rect.x=self.x
+
     def blitme(self):
         self.screen.blit(self.image,self.rect)
+
+    def check_edges(self):
+        screen_rect=self.screen.get_rect()
+        if self.rect.right>=screen_rect.right:
+            return True
+        elif self.rect.right<0:
+            return True
--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,9 @@
 import pygame
 from settings import Settings
 from ship import Ship
+from pygame.sprite import Group
+from bullet import Bullet
+from alien import Alien
 import game_function as gf
 def run_game():
     pygame.init()
@@ -8,10 +11,15 @@ def run_game():
     screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien")
     ship=Ship(ai_settings,screen)
-
+    bullets=Group()
+    alien=Alien(ai_settings,screen)
+    aliens=Group()
+    gf.creat_fleet(ai_settings,screen,ship,aliens)
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings,screen,ship,bullets)
         ship.update() #根据标志状态实时更新飞船位置
-        gf.update_screen(ai_settings,screen,ship)
+        gf.update_bullets(bullets)
+        gf.update_aliens(ai_settings,aliens)
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 
 run_game()
